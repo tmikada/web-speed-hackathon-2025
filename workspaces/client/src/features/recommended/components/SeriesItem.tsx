919f4e44fd49fbe4d409aa3e808572596d220cb2
tmikada <29991871+tmikada@users.noreply.github.com>
2026-02-11 16:23:22 +0900

    カルーセルの全アイテムが即座にレンダリングされている

--- workspaces/client/src/features/recommended/components/SeriesItem.tsx
+++ workspaces/client/src/features/recommended/components/SeriesItem.tsx
@@ -21,7 +21,7 @@ export const SeriesItem = ({ series }: Props) => {
             <>
               <div className="relative overflow-hidden rounded-[8px] border-[2px] border-solid border-[#FFFFFF1F]">
                 <Flipped stagger flipId={isTransitioning ? `series-${series.id}` : 0}>
-                  <img alt="" className="h-auto w-full" src={series.thumbnailUrl} />
+                  <img alt="" className="h-auto w-full" src={series.thumbnailUrl} loading="lazy" />
                 </Flipped>
               </div>
               <div className="p-[8px]">
