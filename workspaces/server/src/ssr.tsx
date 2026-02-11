37fa2ba1cab1de9444128e096cb7c4626752e17a
tmikada <29991871+tmikada@users.noreply.github.com>
2026-02-11 22:25:51 +0900

    main.jsの読み込みブロック解除

--- workspaces/server/src/ssr.tsx
+++ workspaces/server/src/ssr.tsx
@@ -53,7 +53,7 @@ export function registerSsr(app: FastifyInstance): void {
         <head>
           <meta charSet="UTF-8" />
           <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-          <script src="/public/main.js"></script>
+          <script defer src="/public/main.js"></script>
           </head>
         <body></body>
       </html>
