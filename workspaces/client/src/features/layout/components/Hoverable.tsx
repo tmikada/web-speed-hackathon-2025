f49b3c8a0dc5059ac01bda0716e671a2ea58cb58
tmikada <29991871+tmikada@users.noreply.github.com>
2026-02-13 20:52:14 +0900

    Hoverable改善

--- workspaces/client/src/features/layout/components/Hoverable.tsx
+++ workspaces/client/src/features/layout/components/Hoverable.tsx
@@ -1,8 +1,5 @@
 import classNames from 'classnames';
-import { Children, cloneElement, ReactElement, Ref, useRef } from 'react';
-import { useMergeRefs } from 'use-callback-ref';
-
-import { usePointer } from '@wsh-2025/client/src/features/layout/hooks/usePointer';
+import { Children, cloneElement, ReactElement, Ref } from 'react';
 
 interface Props {
   children: ReactElement<{ className?: string; ref?: Ref<unknown> }>;
@@ -14,26 +11,19 @@ interface Props {
 
 export const Hoverable = (props: Props) => {
   const child = Children.only(props.children);
-  const elementRef = useRef<HTMLDivElement>(null);
 
-  const mergedRef = useMergeRefs([elementRef, child.props.ref].filter((v) => v != null));
-
-  const pointer = usePointer();
-  const elementRect = elementRef.current?.getBoundingClientRect();
-
-  const hovered =
-    elementRect != null &&
-    elementRect.left <= pointer.x &&
-    pointer.x <= elementRect.right &&
-    elementRect.top <= pointer.y &&
-    pointer.y <= elementRect.bottom;
+  const hoveredClasses = props.classNames.hovered
+    ?.split(/\s+/)
+    .filter(Boolean)
+    .map((cls) => `hover:${cls}`)
+    .join(' ');
 
   return cloneElement(child, {
     className: classNames(
       child.props.className,
       'cursor-pointer',
-      hovered ? props.classNames.hovered : props.classNames.default,
+      props.classNames.default,
+      hoveredClasses,
     ),
-    ref: mergedRef,
   });
 };
