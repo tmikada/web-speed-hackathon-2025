90fa785798b818138a7f21d4cd0c928343db99f5
tmikada <29991871+tmikada@users.noreply.github.com>
2026-02-14 11:34:28 +0900

    pointermove 不要なポーリングループの除去

--- workspaces/client/src/features/layout/hooks/useSubscribePointer.ts
+++ workspaces/client/src/features/layout/hooks/useSubscribePointer.ts
@@ -8,20 +8,10 @@ export function useSubscribePointer(): void {
   useEffect(() => {
     const abortController = new AbortController();
 
-    const current = { x: 0, y: 0 };
     const handlePointerMove = (ev: MouseEvent) => {
-      current.x = ev.clientX;
-      current.y = ev.clientY;
+      s.features.layout.updatePointer({ x: ev.clientX, y: ev.clientY });
     };
     window.addEventListener('pointermove', handlePointerMove, { signal: abortController.signal });
-
-    let immediate = setImmediate(function tick() {
-      s.features.layout.updatePointer({ ...current });
-      immediate = setImmediate(tick);
-    });
-    abortController.signal.addEventListener('abort', () => {
-      clearImmediate(immediate);
-    });
 
     return () => {
       abortController.abort();
