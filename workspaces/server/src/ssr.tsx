fe398966e9683d1f9610e97a5f2aacbb06efa00e
tmikada <29991871+tmikada@users.noreply.github.com>
2026-02-14 17:29:08 +0900

    cssレンダリングブロック

--- workspaces/server/src/ssr.tsx
+++ workspaces/server/src/ssr.tsx
@@ -53,7 +53,8 @@ export function registerSsr(app: FastifyInstance): void {
         <head>
           <meta charSet="UTF-8" />
           <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-          <link rel="stylesheet" href="/public/main.css" />
+          <link rel="preload" href="/public/main.css" as="style" />
+          <link rel="stylesheet" href="/public/main.css" media="print" onload="this.media='all'" />
           <script defer src="/public/main.js"></script>
           </head>
         <body></body>
