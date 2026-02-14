2c7b1f4b86008e54f7b4ed60efc175a248f02a5e
tmikada <29991871+tmikada@users.noreply.github.com>
2026-02-14 11:33:15 +0900

    ReDoS対応

--- workspaces/client/src/features/auth/logics/isValidEmail.ts
+++ workspaces/client/src/features/auth/logics/isValidEmail.ts
@@ -1,2 +1,2 @@
-const EMAIL_REGEX = /^([A-Z0-9_+-]+\.?)*[A-Z0-9_+-]@([A-Z0-9][A-Z0-9-]*\.)+[A-Z]{2,}$/i;
+const EMAIL_REGEX = /^[A-Z0-9_+-]+(\.[A-Z0-9_+-]+)*@([A-Z0-9][A-Z0-9-]*\.)+[A-Z]{2,}$/i;
 export const isValidEmail = (email: string): boolean => EMAIL_REGEX.test(email);
